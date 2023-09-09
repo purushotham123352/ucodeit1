de8386c135b12f2844d6028ed4eb02078fe9c0d2
purushotham123352 <86943197+purushotham123352@users.noreply.github.com>
2023-09-09 07:27:17 +0530

    initial commit

---
+++ firstjdbc/src/main/java/com/spring/firstjdbc/App.java
@@ -0,0 +1,21 @@
+package com.spring.firstjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@SpringBootConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println();
+        SpringApplication.run(App.class, args);
+    }
+}
